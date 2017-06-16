939ee0f92026c79c487d5c1cf0c2755ced669a30
Atahualpa Silva F <atahualpasilva@gmail.com>
2017-06-16 12:42:40 -0400

    Add project files.

---
+++ DrWebG8Reporte/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DrWebG8Reporte.Startup))]
+namespace DrWebG8Reporte
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
